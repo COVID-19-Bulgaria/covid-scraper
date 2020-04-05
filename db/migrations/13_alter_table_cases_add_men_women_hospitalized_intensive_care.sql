ef2041063d096c6e4d86016192ea2923fa806a5a
Veselin Stoyanov <veselin1996@gmail.com>
2020-04-06 02:31:03 +0300

    Add medical_staff column

--- db/migrations/13_alter_table_cases_add_men_women_hospitalized_intensive_care.sql
+++ db/migrations/13_alter_table_cases_add_men_women_hospitalized_intensive_care.sql
@@ -3,6 +3,7 @@ ADD COLUMN men integer,
 ADD COLUMN women integer,
 ADD COLUMN hospitalized integer,
 ADD COLUMN intensive_care integer,
+ADD COLUMN medical_staff integer,
 ADD COLUMN country_id integer,
 ADD FOREIGN KEY ("country_id") REFERENCES "covid19"."countries" ("id");
 
