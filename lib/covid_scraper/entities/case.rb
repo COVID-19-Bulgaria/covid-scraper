66031aa8f9dd10ca1559c236dfc4bd4fe7412aac
Veselin Stoyanov <veselin1996@gmail.com>
2020-06-07 20:52:35 +0300

    Make unstable data optional.

--- lib/covid_scraper/entities/case.rb
+++ lib/covid_scraper/entities/case.rb
@@ -3,18 +3,18 @@
 module CovidScraper
   module Entities
     class Case < ROM::Struct
-      attribute? :id, Types::Coercible::Integer
+      attribute? :id, Types::Coercible::Integer.optional
       attribute :country_id, Types::Coercible::Integer
       attribute :infected, Types::Coercible::Integer
       attribute :cured, Types::Coercible::Integer
       attribute :fatal, Types::Coercible::Integer
-      attribute? :men, Types::Coercible::Integer
-      attribute? :women, Types::Coercible::Integer
-      attribute? :hospitalized, Types::Coercible::Integer
-      attribute? :intensive_care, Types::Coercible::Integer
-      attribute? :medical_staff, Types::Coercible::Integer
-      attribute? :pcr_tests, Types::Coercible::Integer
-      attribute? :sources, Types::Coercible::String
+      attribute? :men, Types::Coercible::Integer.optional
+      attribute? :women, Types::Coercible::Integer.optional
+      attribute? :hospitalized, Types::Coercible::Integer.optional
+      attribute? :intensive_care, Types::Coercible::Integer.optional
+      attribute? :medical_staff, Types::Coercible::Integer.optional
+      attribute? :pcr_tests, Types::Coercible::Integer.optional
+      attribute? :sources, Types::Coercible::String.optional
       attribute :timestamp, Types::Nominal::Time
     end
   end
